9d32c013c12f5c98edcb4cacaa34cd7d3bf81aa5
Monika Gopi <144507826+monikagopi03@users.noreply.github.com>
2025-05-27 21:38:01 +0530

    Add files via upload

---
+++ SQL/4. Peak Session Hours (10 AM to 12 PM).sql
@@ -0,0 +1,5 @@
+SELECT e.title, COUNT(*) AS session_count
+FROM Sessions s
+JOIN Events e ON s.event_id = e.event_id
+WHERE TIME(s.start_time) BETWEEN '10:00:00' AND '12:00:00'
+GROUP BY e.event_id;
